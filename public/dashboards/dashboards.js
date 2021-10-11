6d6b1e90abbb826f318198d82fb301c50616801b
Chertanov Denis <dchertanov@croc.ru>
2021-10-11 22:48:52 +0300

    Add something strange

---
+++ public/dashboards/dashboards.js
@@ -0,0 +1,11 @@
+let accountName = 'Vasya Pupkin';
+let firstDashboard = 'University';
+let secondDashboard = 'Work';
+
+
+const accountButton = document.getElementById('accountButton');
+accountButton.innerHTML = '<i class="far fa-user"></i> ' + accountName;
+
+// accountButton.addEventListener('click', function () {
+
+// });
--- server/server.js
+++ server/server.js
@@ -9,8 +9,26 @@ const server = http.createServer(function (request, response) {
     } else if (request.url === '/images/to-do.png') {
         const img = fs.readFileSync('./public/images/to-do.png');
         response.end(img);
-    } else {
+    } else if (request.url === '/index.html') {
         const html = fs.readFileSync('./public/index.html');
+        response.end(html);
+    } else if (request.url === '/test.js') {
+        const js = fs.readFileSync('./public/test/test.js');
+        response.end(js);
+    } else if (request.url === '/main.js') {
+        const js = fs.readFileSync('./public/main.js');
+        response.end(js);
+    } else if (request.url === '/dashboards.html') {
+        const html = fs.readFileSync('./public/dashboards/dashboards.html');
+        response.end(html);
+    } else if (request.url === '/dashboards.css') {
+        const css = fs.readFileSync('./public/dashboards/dashboards.css');
+        response.end(css);
+    } else if (request.url === '/dashboards.js') {
+        const js = fs.readFileSync('./public/dashboards/dashboards.js');
+        response.end(js);
+    } else {
+        const html = fs.readFileSync('./public/test/test.html');
         response.end(html);
     }
 });
